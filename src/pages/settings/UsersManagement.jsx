ccd4b2369eba7247c10bdbd9fb7faf2c15e53e1e
Sergii <sergeybotl@gmail.com>
2025-11-25 00:13:27 +0200

    change name enabled

--- src/pages/settings/UsersManagement.jsx
+++ src/pages/settings/UsersManagement.jsx
@@ -118,7 +118,6 @@ export default function UsersManagement({
                     value={selectedUser.name || ''}
                     onChange={(e) => handleInputChange(selectedUserId, 'name', e.target.value)}
                     placeholder="Ім'я користувача"
-                    disabled
                   />
                 </div>
 
